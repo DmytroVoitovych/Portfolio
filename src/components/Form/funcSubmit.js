df498a9df38706ae26d34cdf82d3e4f909f1d9dd
DmytroVoitovych <begunec23@gmail.com>
2022-12-15 18:56:15 +0200

    change back

--- src/components/Form/funcSubmit.js
+++ src/components/Form/funcSubmit.js
@@ -1,14 +1,15 @@
 import { Block, Report } from 'notiflix';
 import { optLoader } from './optionLoader';
 import { funcClickSuccess } from './funcClickSuccess';
-import { form } from './ContactMe.styled';
 import axios from 'axios';
 const {REACT_APP_API_URL} = process.env;
 
-export const funcSubmit = async (data,onClose,setChange,reset) => {
-        Block.dots(`.css-${form.name}`, 'The form is being submitted...', optLoader );
-        try {
-           await axios.post(REACT_APP_API_URL,data);
+export const funcSubmit = async (data, onClose, setChange, reset) => {
+   
+        Block.dots(`.css-1w6kkf8`, 'The form is being submitted...', optLoader );
+   
+    try {
+            await axios.post(REACT_APP_API_URL,data);
             await reset();
             await Report.success('Success','Thanks for your feedback)))');
             funcClickSuccess(onClose,setChange);
@@ -18,6 +19,6 @@ export const funcSubmit = async (data,onClose,setChange,reset) => {
              Report.failure('Error',`${err}`);
         }
         finally {
-           Block.remove(`.css-${form.name}`); 
+           Block.remove(`.css-1w6kkf8`); 
         }
     };
