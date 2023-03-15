569b2b21c8baea7fb1c410accb9b4b133a9991fd
DmytroVoitovych <begunec23@gmail.com>
2023-03-15 15:11:42 +0200

    change description hero section

--- src/components/hero/helpers/description.js
+++ src/components/hero/helpers/description.js
@@ -1,18 +1,16 @@
+const aboutMe = `I'm passionate about learning and exploring new things in frontend
+development. My interest has only grown stronger since I started in January
+2022. I've enjoyed working on various projects, gaining experience and
+knowledge of popular libraries and frameworks such as React.js, Vue.js, Node.js,
+Axios, Notiflix, and Lodash.`;
 
-
-const aboutMe = `I have a mad interest in learning something new about programming
-                    and learning new things. I started to learn frontend in January of
-                    this year and since then my interest has not dropped one bit. 
-                    It's a great fun for me to work on different projects and tasks,
-                    get new experience and gain knowledge about libraries and frameworks like React.js, Axios, Notiflix, and of course Lodash.`;
-
-const index = aboutMe.indexOf(`It's a great`);
+const index = aboutMe.indexOf(`I've enjoyed`);
 const subOne = aboutMe.slice(0, index);
 const subTwo = aboutMe.slice(index);
-const linkResume = 'https://drive.google.com/file/d/1EHzLZAhZ12UxDCn4sMabCXPj3fH2iAUL/view?usp=sharing' ;
+const linkResume =
+  'https://drive.google.com/file/d/12ab8Te2ZJcloTA1vgLMktVDGlZpTLArb/view?usp=share_link';
 
-const nodeDesc = 'Apart from the fact that I am applying for a job as a frontend developer, I am ready to consider working as a backend developer, because I have experience with Node.js and Mongo DB. ';
+const nodeDesc =
+  'Apart from the fact that I am applying for a job as a frontend developer, I am ready to consider working as a backend developer, because I have experience with Node.js and Mongo DB. ';
 
-
-
-export {subOne, subTwo, linkResume, nodeDesc };
+export { subOne, subTwo, linkResume, nodeDesc };
